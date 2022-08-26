9fcec11637ed9c7da3327a05abee4a857e0ec359
Kelly <kellyshao728@gmail.com>
2022-08-26 14:37:54 -0700

    Login Setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
-const { restoreUser } = require('../../utils/auth.js');
+
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
+module.exports = router;
 
 
 // TEST
@@ -45,4 +54,3 @@ router.use(restoreUser);
 //   }
 // );
 
-module.exports = router;
