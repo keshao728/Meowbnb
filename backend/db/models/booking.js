09541e392860a4b2d9fe3ae71242d1c600cf45cc
Kelly <kellyshao728@gmail.com>
2022-08-29 23:56:02 -0700

    Add Review Migration

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -4,13 +4,8 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
       Booking.belongsTo(models.User, {foreignKey: 'id'});
       Booking.belongsTo(models.Spot, {foreignKey: 'id'})
     }
