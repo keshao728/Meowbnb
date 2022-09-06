66780b68d75887812cd1dc23a28f59c5293b643b
Kelly <kellyshao728@gmail.com>
2022-09-06 08:06:40 -0700

    update

--- backend/db/migrations/20220830050000-create-spot.js
+++ backend/db/migrations/20220830050000-create-spot.js
@@ -51,7 +51,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, {freezeTableName: true});
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
