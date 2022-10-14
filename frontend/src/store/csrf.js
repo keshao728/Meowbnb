1618daad462ba1b61f44a4b1bae9b705aab0414b
Kelly <kellyshao728@gmail.com>
2022-10-13 22:26:18 -0700

    Restore the XSRF-TOKEN cookie

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -27,6 +27,7 @@ export async function csrfFetch(url, options = {}) {
   return res;
 }
 
+// call this to get the "XSRF-TOKEN" cookie, should only be used in development
 export function restoreCSRF() {
   return csrfFetch('/api/csrf/restore');
 }
