905df2ef9cb4ae26844f3f67e4116b4602964a52
Kelly <kellyshao728@gmail.com>
2022-10-16 00:20:17 -0700

    Set Up Logout & Navigation component

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -50,6 +50,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
