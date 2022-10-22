d5ec164044c92611898b618002a32b4f9b5901a8
Kelly <kellyshao728@gmail.com>
2022-10-21 23:45:20 -0700

    Handle Validation Error Message

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -13,22 +13,22 @@ const validateSignup = [
     // .exists({ checkFalsy: true })
     // .withMessage('First Name is required')
     .isLength({ min: 2 })
-    .withMessage('First name must be more than 2 characters.'),
+    .withMessage('First name must be more than 2 characters'),
   check('lastName')
     // .exists({ checkFalsy: true })
     // .withMessage('Last Name is required')
     .isLength({ min: 2 })
-    .withMessage('Last name must be more than 2 characters.'),
+    .withMessage('Last name must be more than 2 characters'),
   check('email')
     // .exists({ checkFalsy: true })
     // .withMessage('Invalid email')
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Please provide a valid email'),
   check('username')
     // .exists({ checkFalsy: true })
     // .withMessage('Username is required')
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
+    .withMessage('Please provide a username with at least 4 characters'),
   // check('username')
   //   .isLength({ min: 2 })
   //   .withMessage('Please provide a username with at least 2 characters.'),
@@ -38,7 +38,7 @@ const validateSignup = [
   check('password')
     // .exists({ checkFalsy: true })
     .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
+    .withMessage('Password must be 6 characters or more'),
   handleValidationErrors
 ];
 
