28d2b904a313c183b35cdc0133397792fac20d35
Kelly <kellyshao728@gmail.com>
2022-10-22 19:48:19 -0700

    Single Spot CSS Display

--- frontend/src/components/SingleSpotBrowser/index.js
+++ frontend/src/components/SingleSpotBrowser/index.js
@@ -50,61 +50,101 @@ const SingleSpotBrowser = () => {
       <div className="browser-wrapper">
         {currSpot.id &&
           <div className="single-spot-parent">
-            <strong className="spot-name">
-              {currSpot.name}
-            </strong>
+            <div>
+              <strong className="spot-name">
+                {currSpot.name}
+              </strong>
 
-            <div className="spot-details">
-              <div className="spot-star">
-                <i className="fa-solid fa-paw"></i>
-                &nbsp;
-                {currSpot.avgStarRating > 0 ? Number(currSpot.avgStarRating).toFixed(2) : 'New'}
-                &nbsp;&nbsp;·
-              </div>
+              <div className="spot-details">
+                <div className="spot-star">
+                  <i className="fa-solid fa-paw"></i>
+                  &nbsp;
+                  {currSpot.avgStarRating > 0 ? Number(currSpot.avgStarRating).toFixed(2) : 'New'}
+                  &nbsp;&nbsp;·
+                </div>
 
-              <div>&nbsp;&nbsp;{currSpot.numReviews} reviews&nbsp;&nbsp;·&nbsp;&nbsp;</div>
-              <div className="spot-address" key={currSpot.name}>
-                {currSpot.city}, {currSpot.state}, {currSpot.country}
-              </div>
-            </div>
-
-            <div className="all-images">
-              <img className="big-image" alt="spot" src={currSpot.SpotImages[0]?.url}></img>
-            </div>
-
-            <div className="spot-hosted">
-              <div className="hosted-by"> Spot hosted by {currSpot.Owner.firstName} {currSpot.Owner.lastName}</div>
-              {/* {console.log(currSpot)} */}
-              <img className='hosted-pic' src="https://drive.google.com/uc?export=view&id=1p7ALHdhdZsKxbR6qaFb4Wd_rbqSy1DwI" alt="Meowbnb Default Profile"></img>
-            </div>
-
-            {/* <div className="check-in-super"> */}
-            <div className="self-check-in">
-              <i className="fa-solid fa-door-open"></i>
-              <div className="check-in">
-                <strong> Self check-in </strong>
-                <div>
-                  Check yourself in with the lockbox.
+                <div>&nbsp;&nbsp;{currSpot.numReviews} reviews&nbsp;&nbsp;·&nbsp;&nbsp;</div>
+                <div className="spot-address" key={currSpot.name}>
+                  {currSpot.city}, {currSpot.state}, {currSpot.country}
                 </div>
               </div>
-            </div>
 
-            <div className="self-check-in" id="super">
-              <i class="fa-regular fa-id-badge"></i>
-              <div className="superhost">
-                <strong> {currSpot.Owner.firstName} is a Superhost </strong>
-                <div> Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</div>
+              <div className="all-images">
+                <img className="big-image" alt="spot" src={currSpot.SpotImages[0]?.url}></img>
               </div>
             </div>
-            {/* </div> */}
+            <div className="middle-spot-items">
+              <div>
+                <div className="spot-hosted">
+                  <div className="hosted-by"> Spot hosted by {currSpot.Owner.firstName} {currSpot.Owner.lastName}</div>
+                  {/* {console.log(currSpot)} */}
+                  <img className='hosted-pic' src="https://drive.google.com/uc?export=view&id=1p7ALHdhdZsKxbR6qaFb4Wd_rbqSy1DwI" alt="Meowbnb Default Profile"></img>
+                </div>
 
-          <div className="aircover-parent">
-            <img className="aircover-pic" src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" />
-          <div className="aircover-des">Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</div>
-          </div>
-            <div>
-              <div className="spot-description">
-                {currSpot.description}
+                {/* <div className="check-in-super"> */}
+                <div className="self-check-in">
+                  <i className="fa-solid fa-door-open"></i>
+                  <div className="check-in">
+                    <strong> Self check-in </strong>
+                    <div>
+                      Check yourself in with the lockbox.
+                    </div>
+                  </div>
+                </div>
+
+                <div className="self-check-in" id="super">
+                  <i class="fa-regular fa-id-badge"></i>
+                  <div className="superhost">
+                    <strong> {currSpot.Owner.firstName} is a Superhost </strong>
+                    <div> Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</div>
+                  </div>
+                </div>
+                {/* </div> */}
+
+                <div className="aircover-parent">
+                  <img className="aircover-pic" src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" />
+                  <div className="aircover-des">Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</div>
+                </div>
+                <div>
+                  <div className="spot-description">
+                    {currSpot.description}
+                  </div>
+                </div>
+              </div>
+
+              <div className="spot-price-mother">
+                <div className="spot-price">
+                  <div className="spot-price-child">
+                    <strong className="price-per-night">
+                      ${currSpot.price}
+                    </strong>
+                    &nbsp;/&nbsp;night
+                  </div>
+                  {allowReviewAction &&
+                    <div className='review-this-spot'>
+                      <NavLink className="review-click"
+                        to={`/spots/${currSpot.id}/review`}>
+                        Review This Spot
+                      </NavLink>
+                    </div>
+                  }
+                  {!allowReviewAction && sessionUser && !alreadyReviewed &&
+                    <div className='review-this-spot'>
+                      <NavLink className="review-click"
+                        to="/spots/my-spots">
+                        Manage My Spot
+                      </NavLink>
+                    </div>
+                  }
+                  {alreadyReviewed &&
+                    <div className='review-this-spot'>
+                      <NavLink className="review-click"
+                        to="/spots/my-reviews">
+                        Manage My Reviews
+                      </NavLink>
+                    </div>
+                  }
+                </div>
               </div>
             </div>
           </div>
@@ -143,44 +183,9 @@ const SingleSpotBrowser = () => {
             ))}
           </div>
         </div>
-
-      </div>
-
-      <div className="spot-price-mother">
-        <div className="spot-price">
-          <div className="spot-price-child">
-            <strong className="price-per-night">
-              ${currSpot.price}
-            </strong>
-            &nbsp;/&nbsp;night
-          </div>
-          {allowReviewAction &&
-            <div className='review-this-spot'>
-              <NavLink className="review-click"
-                to={`/spots/${currSpot.id}/review`}>
-                Review This Spot
-              </NavLink>
-            </div>
-          }
-          {!allowReviewAction && sessionUser && !alreadyReviewed &&
-            <div className='review-this-spot'>
-              <NavLink className="review-click"
-                to="/spots/my-spots">
-                Manage My Spot
-              </NavLink>
-            </div>
-          }
-          {alreadyReviewed &&
-            <div className='review-this-spot'>
-            <NavLink className="review-click"
-              to="/spots/my-reviews">
-              Manage My Reviews
-            </NavLink>
-          </div>
-          }
-        </div>
       </div>
     </div>
+
   )
 }
 
