78d147b14a1f389d895420d90845d8d7ae5d54e2
Kelly <kellyshao728@gmail.com>
2023-01-08 06:31:54 -0800

    Booking display

--- frontend/src/components/Booking/index.js
+++ frontend/src/components/Booking/index.js
@@ -31,94 +31,100 @@ const UserBookings = () => {
         <div className="user-booking-des">Upcoming reservations</div>
 
         <div className="individual-bookings-wrapper">
+
+
           {bookingsArr?.map((booking) => (
 
-            <NavLink to={`/spots/${booking.spotId}`} className="individual-booking">
+            <div className="user-booking-wrapper-1">
+              <NavLink to={`/spots/${booking.spotId}`} className="individual-booking">
 
-              <div className="individual-booking-left">
-                <div className="booking-left-top">
-                  <div className="booking-name">{booking.Spot.name}</div>
-                </div>
+                <div className="individual-booking-left">
+                  <div className="booking-left-top">
+                    <div className="booking-name">{booking.Spot.name}</div>
+                  </div>
 
-                <div className="booking-left-bottom">
+                  <div className="booking-left-bottom">
 
-                  <div className="booking-dates">
-                    {moment(booking.startDate).format('YYYY') === moment(booking.endDate).format('YYYY') && moment(booking.startDate).format('MMM') === moment(booking.endDate).format('MMM') ?
-                      <div className="bookding-individual-dates">
-                        <div>
-                          {moment(booking.startDate).format('MMM')}
-                        </div>
-                        <div className="booking-specific-date">
-                          <div>
-                            {moment(booking.startDate).format('D')}
-                          </div>
-                          -
-                          <div>
-                            {moment(booking.endDate).format('D')}
-                          </div>
-                        </div>
-                        <div className="booking-specific-yr">
-                          {moment(booking.endDate).format('YYYY')}
-                        </div>
-                      </div> :
-                      moment(booking.startDate).format('YYYY') === moment(booking.endDate).format('YYYY') ?
+                    <div className="booking-dates">
+                      {moment(booking.startDate).format('YYYY') === moment(booking.endDate).format('YYYY') && moment(booking.startDate).format('MMM') === moment(booking.endDate).format('MMM') ?
                         <div className="bookding-individual-dates">
+                          <div>
+                            {moment(booking.startDate).format('MMM')}
+                          </div>
                           <div className="booking-specific-date">
                             <div>
-                              {moment(booking.startDate).format('MMM D')} -
+                              {moment(booking.startDate).format('D')}
                             </div>
+                            -
                             <div>
-                              {moment(booking.endDate).format('MMM D')}
+                              {moment(booking.endDate).format('D')}
                             </div>
                           </div>
                           <div className="booking-specific-yr">
                             {moment(booking.endDate).format('YYYY')}
                           </div>
                         </div> :
-                        <div className="bookding-individual-dates">
-                          <div className="booking-specific-date">
-                            <div>
-                              {moment(booking.startDate).format('MMM D YYYY')} -
+                        moment(booking.startDate).format('YYYY') === moment(booking.endDate).format('YYYY') ?
+                          <div className="bookding-individual-dates">
+                            <div className="booking-specific-date">
+                              <div>
+                                {moment(booking.startDate).format('MMM D')} -
+                              </div>
+                              <div>
+                                {moment(booking.endDate).format('MMM D')}
+                              </div>
                             </div>
-                            <div>
-                              {moment(booking.endDate).format('MMM D YYYY')}
+                            <div className="booking-specific-yr">
+                              {moment(booking.endDate).format('YYYY')}
+                            </div>
+                          </div> :
+                          <div className="bookding-individual-dates">
+                            <div className="booking-specific-date">
+                              <div>
+                                {moment(booking.startDate).format('MMM D YYYY')} -
+                              </div>
+                              <div>
+                                {moment(booking.endDate).format('MMM D YYYY')}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                    }
-                  </div>
-                  <div className="booking-address-wrapper">
-                    <div className="booking-address-container">
-                      <div>
-                        {booking.Spot.address}
-                      </div>
-                      <div className="booking-specific-city-state">
+                      }
+                    </div>
+                    <div className="booking-address-wrapper">
+                      <div className="booking-address-container">
                         <div>
-                          {booking.Spot.city},
+                          {booking.Spot.address}
                         </div>
-                        <div>
-                          {booking.Spot.state}
+                        <div className="booking-specific-city-state">
+                          <div>
+                            {booking.Spot.city},
+                          </div>
+                          <div>
+                            {booking.Spot.state}
+                          </div>
                         </div>
-                      </div>
-                      <div className="booking-specific-country">
-                        {booking.Spot.country}
+                        <div className="booking-specific-country">
+                          {booking.Spot.country}
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="individual-booking-right">
-                <img className="booking-img" src={booking.Spot.previewImage} />
-                <div className="booking-upcoming-days">{moment().to(moment(booking.startDate))}</div>
+                <div className="individual-booking-right">
+                  <img className="booking-img" src={booking.Spot.previewImage} />
+                  <div className="booking-upcoming-days">{new moment().to(moment(booking.startDate))}</div>
+                </div>
+
+
+              </NavLink>
+              <div>
                 <button className="delete-booking-button"
                   onClick={() => dispatch(deleteBookingThunk(booking.id))}>
                   <i className="fa-regular fa-trash-can"></i>
                 </button>
               </div>
-
-
-            </NavLink>
+            </div>
           ))}
         </div>
       </div>
